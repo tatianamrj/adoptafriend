4d7141d225358d0cd0606b33ec90601fbef7922e
Tatiana Johnson <tatianamrjsn@gmail.com>
2018-05-11 12:00:41 -0400

    added results functionality

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-let petFinderApi = "https://api.petfinder.com/pet.find?key=d709360a7a61accf370002fcfd477c15&format=json"
+let petFinderApi = "https://api.petfinder.com/pet.find"
 
 function watchSubmit() {
   $('#form').submit(event => {
     event.preventDefault();
-    let zipcodeSubmit = $('#zipcode').val();
-    petFinderApi+="&location="+zipcodeSubmit;
-    
-    
+    let zipcode = $('#zipcode').val();
+    // petFinderApi+="&location="+zipcodeSubmit;
+
     $.ajax({
       dataType:"jsonp",
       url:petFinderApi,
       type:"get",
+      data: {
+        key: "d709360a7a61accf370002fcfd477c15",
+        location: zipcode,
+        format: "json"
+      },
       success: function(data){
-      console.log(data);
+        console.log(data);
+        let petResults= data.petfinder.pets.pet.map(function (pet){
+          return `<h1>${pet.name.$t}</h1>`
+        })
+        $("#results").html(petResults);
+      },
+      error: function (error) {
+        console.log(error);
+        // body...
       }
     });
     // clear out the input
     $('#zipcode').val("");
 
   });
-  
 }
-  watchSubmit();
+  function getDataFromApi(searchTerm, callback) {
+  // const settings = {
+  //   url: petFinderApi,
+  //  
+  //   dataType: 'json',
+  //   type: 'GET',
+  //   success: callback
+};
+
+  // $.ajax(settings);
+
+watchSubmit();
