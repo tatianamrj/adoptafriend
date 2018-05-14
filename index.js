98cee038b62f156d367ee5c252a434484ee82674
Tatiana Johnson <tatianamrjsn@gmail.com>
2018-05-14 15:43:19 -0400

    added user stories

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// PetFinder API where the data is gathered from
+
 let petFinderApi = "https://api.petfinder.com/pet.find";
-// when user submit zipcode information
+
+// Load search function
 
 function loadSearch() {
 const homePage = `<div id="home-page"><title>Home</title><form id="pet-form"><label for="zipcode"></label><input type="number" id="zipcode" placeholder="ZipCode"><label for="pet-type"></label><select id="pet-type"><option value="">Select Animal</option><option value="barnyard">Barnyard Animal</option><option value="bird">Bird</option><option value="cat">Cat</option><option value="dog">Dog</option><option value="horse">Horse</option><option value="Reptile">Reptile</option><option value="smallfurry">Small and Furry</option></select><label for="sex"></label><input type="radio" name="sex" value="M">Male<input type="radio" name="sex" value="F">Female<input type="submit" value="submit"></form><p> some content that describes your project. </p> </div><div id="results-page" class="hidden"><section> <h2>Results</h2></section><div id="results"></div> </div><div id="single-pet-page" class="hidden"><p class="pet-info"> Husky, Age: 12</p><div id="large-image"></div><div id="map"></div></div><footer id="restart">About me...</footer>`;
@@ -8,6 +11,8 @@ $("main").html(homePage);
 
 loadSearch();
 
+// User Submits zipcode and other information
+
 function watchSubmit() {
   $("#pet-form").submit(event => {
     event.preventDefault();
@@ -15,6 +20,7 @@ function watchSubmit() {
     let petType = $("#pet-type").val();
     let sex = $("input[name=sex]:checked").val();
 
+// first ajax call for location, animal and sex formmated in json
     $.ajax({
       dataType: "jsonp",
       url: petFinderApi,
@@ -39,6 +45,7 @@ function watchSubmit() {
   });
 }
 
+// petresults page loads
 function showResults(pets) {
   $("#home-page").hide();
   $("#results-page").show();
@@ -68,6 +75,8 @@ function showResults(pets) {
   
   $("#results").html(petResults);
 }
+
+// user clicks on image to get more information and map with pet location
 
 function imageClick() {
   $("#results-page").on('click', '.pet', event => {
