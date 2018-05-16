a046b5ca6392d467611d71faed8938692bdbed0a
Tatiana Johnson <tatianamrjsn@gmail.com>
2018-05-16 16:21:31 -0400

    restart button

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let petFinderApi = "https://api.petfinder.com/pet.find";
 function loadSearch() {
 const homePage = `<div id="home-page" class="col-3"><title>Home</title><form id="pet-form"><label for="zipcode"></label><input id="zipcode" placeholder="ZipCode"><label for="pet-type"></label><select id="pet-type"><option value="">Select Animal</option><option value="barnyard">Barnyard Animal</option><option value="bird">Bird</option><option value="cat">Cat</option><option value="dog">Dog</option><option value="horse">Horse</option><option value="Reptile">Reptile</option><option value="smallfurry">Small and Furry</option></select><label for="sex"><input type="radio" name="sex" value="M" id= "sex-m-f"><label for="male">Male</label><input type="radio" name="sex" value="F"><label for="female">Female</label><input type="submit" value="SUBMIT" id="submit-search"></form><p class="app-info"> Adopt a Friend is an app to help you find adoptable pets near your location. </p> </div><div id="results-page" class="hidden"><section> <h2>Results</h2></section><div id="results"></div> </div><div id="single-pet-page" class="hidden"><p class="pet-info"></p><div id="large-image"></div><div id="map"></div></div><footer id="restart"><div class="row"><div class="col-4"> <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/464184-200.png" alt-text="paw and dog bone"></div><div class="col-4"><img src="https://d30y9cdsu7xlg0.cloudfront.net/png/582073-200.png" alt-text="right pointing arrow"></div><div class="col-4"><img src="https://d30y9cdsu7xlg0.cloudfront.net/png/3966-200.png" alt-text="house-image"></div></div><footer><p class="about-me">About me: My name is Tatiana M.R. Johnson and I am a student in Thinkful's Full Stack Web Development program. <br>I am new to programming, but I love storytelling and I am excited to learn how I can use programming to tell compelling stories.</p></footer>`;
 $("main").html(homePage);
+$("#restart-search").hide();
 }
 
 loadSearch();
@@ -83,17 +84,19 @@ function showResults(pets) {
     return `
     <div class="pet">
       <h1 class="pet-name">${pet.name.$t}</h1>
+      <img src="${petImage}" class="animal-image">
       <p class="animal-type">Type of Animal: ${pet.animal.$t}</p>
       <p class="animal-name">Age: ${pet.age.$t}</p>
       <p class="animal-sex">Sex: ${pet.sex.$t}</p>
       <p class="pet-description hidden">
-      Description: ${pet.description.$t}</p>
-      <img src="${petImage}" class="animal-image">
-    </div><button><a href='index.html'>RESTART SEARCH</button>`;
+      Description: ${pet.description.$t}</p></div>`
   });
   console.log(petResults);
   
   $("#results").html(petResults);
+  $(".row").hide();
+  $("#restart-search").show();
+
 }
 
 // user clicks on image to get more information and map with pet location
@@ -104,6 +107,7 @@ function imageClick() {
     event.preventDefault();
     $("#results-page").hide();
     $("#single-pet-page").show();
+    $("#restart-search").show();
     $(".pet-info").html(event.currentTarget.children);
     $(".pet-description").toggle("hidden");
     $(".row").toggle("hidden");
